7c291c992d57b9d465a5de261c0a4eda12711030
Niklas Reini <niklas.reini01@gmail.com>
2020-11-20 15:22:54 +0200

    second commit

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import Button from '@material-ui/core/Button';
 
 export default function Carlist() { 
     const[trainings, setTrainings] = useState([]);
@@ -11,6 +12,14 @@ export default function Carlist() {
         .then(response => response.json())
         .then(data => setTrainings(data.content))
     }
+
+    const deleteCustomer = (link) => {
+        if (window.confirm("Do you want to delete training?")){
+            fetch(link, {method: 'DELETE'})
+            .then(res => fetchData())
+            .catch(err => console.error(err))
+           }
+        }
     const columns = [
         {
             Header: 'First name',
@@ -39,7 +48,14 @@ export default function Carlist() {
         {
             Header: 'Phone',
             accessor: 'phone'
-        },      
+        },
+        {
+            sortable: false,
+            filterable: false,
+            width: 100,
+            accessor: 'links[0].href',
+            Cell: row => <Button size='small' color='secondary' onClick={() => deleteCustomer(row.value)}>Delete</Button>
+         },      
     ]
 
     return(
--- src/components/TrainingList.js
+++ src/components/TrainingList.js
@@ -2,15 +2,25 @@ import React, {useState, useEffect} from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import moment from 'moment';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 
 export default function Carlist() { 
     const[trainings, setTrainings] = useState([]);
     useEffect(() => fetchData(), [])
     
     const fetchData = () => {
-        fetch('https://customerrest.herokuapp.com/api/trainings')
+        fetch('https://customerrest.herokuapp.com/gettrainings')  
         .then(response => response.json())
-        .then(data => setTrainings(data.content))
+        .then(data => setTrainings(data))
+    }
+
+    const deleteTraining = (link) => {
+       if (window.confirm("Do you want to delete training?")){
+        fetch(link, {method: 'DELETE'})
+        .then(res => fetchData())
+        .catch(err => console.error(err))
+       }
     }
  
     const columns = [
@@ -28,7 +38,19 @@ export default function Carlist() {
         {
             Header: 'Activity',
             accessor: 'activity'
-        },         
+        }, 
+        {
+            Header: 'Customer',
+            accessor: 'customer',
+            Cell: props => props.value.firstname + ' ' + props.value.lastname                
+        }, 
+        {
+            sortable: false,
+            filterable: false,
+            width: 100,
+            accessor: 'links[0].href',
+            Cell: row => <Button size='small' color='secondary' onClick={() => deleteTraining(row.value)}>Delete</Button>
+         },        
     ]
     return(
         <div>
